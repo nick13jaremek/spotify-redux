b52bbcf73cea10c512e28c826a5c88b26341e0dc
Nick Jaremek <nicolas.jaremek@intelygenz.com>
2015-12-31 10:18:13 +0100

    Updated 'Index' component to show a Bootstrap jumbotron with some welcome text, instead of a lonely and unstyled string.

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,7 +4,17 @@ export default class Index extends Component {
 
   render() {
     return (
-      <div>Index</div>
+      <div className="container">
+        <div className="jumbotron">
+          <h1>Welcome to Spotify-Redux!</h1>
+          <p>This is a sample application programmed using the React-Redux library, accompanied by several more components
+          such as Webpack or React-Router.</p>
+
+          <p>
+          This web application uses the Spotify Web API to fetch artists, albums or songs. Click on any of the buttons on
+          the navigation bar and start having fun!</p>
+        </div>
+      </div>
     );
   }
 }
--- test/components/Index.spec.js
+++ test/components/Index.spec.js
@@ -26,8 +26,30 @@ describe('Index component', () => {
     const {output} = setup();
 
     expect(output.type).to.equal('div');
-    expect(output.props.children).to.be.a('string');
-    expect(output.props.children).to.equal('Index');
+    expect(output.props.className).to.equal('container');
+    expect(output.props.children).to.be.an('object');
+
+    let jumbotron = output.props.children;
+    expect(jumbotron.type).to.equal('div');
+    expect(jumbotron.props.className).to.equal('jumbotron');
+    expect(jumbotron.props.children).to.be.an('array');
+    expect(jumbotron.props.children).to.have.length(3);
+
+    let [header, paragraphOne, paragraphTwo] = jumbotron.props.children;
+
+    expect(header.type).to.equal('h1');
+    expect(header.props.children).to.be.a('string');
+    expect(header.props.children).to.equal('Welcome to Spotify-Redux!');
+
+    expect(paragraphOne.type).to.equal('p');
+    expect(paragraphOne.props.children).to.be.a('string');
+    expect(paragraphOne.props.children).to.equal('This is a sample application programmed using the React-Redux ' +
+      'library, accompanied by several more components such as Webpack or React-Router.');
+
+    expect(paragraphTwo.type).to.equal('p');
+    expect(paragraphTwo.props.children).to.be.a('string');
+    expect(paragraphTwo.props.children).to.equal('This web application uses the Spotify Web API to fetch artists, ' +
+      'albums or songs. Click on any of the buttons on the navigation bar and start having fun!');
   });
 
   it('has not props', () => {
