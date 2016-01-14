0ab9c3a517a1f2d937f9b6229f60a7e5b8f3e42f
Nick Jaremek <nick13jaremek@gmail.com>
2016-01-14 11:41:43 +0100

    Improved 'ArtistCard' tests.

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -4,22 +4,34 @@ import {Router, Link} from 'react-router';
 
 export default class ArtistCard extends Component {
 
+  renderArtistData(artistData) {
+
+    const name = artistData.name ? <h4 className="">{artistData.name}</h4> : null;
+    const popularity = artistData.popularity ? <li><span>Popularity: {artistData.popularity}</span></li> : null;
+    const genre = (artistData.genres && artistData.genres.length > 0) ?  <li><span>Genre: {artistData.genres[0]}</span></li> : null;
+    const link = artistData.id ? <Link to={`/artist/${artistData.id}`} className="btn btn-default btn-xs" role="button">More Info</Link> : null;
+
+    return (
+      <div className="caption">
+        {name}
+        <ul>
+          {popularity}
+          {genre}
+        </ul>
+        {link}
+      </div>
+  );
+  }
+
   render() {
     const {artist} = this.props;
-    const imageUrl = (artist.images.length ? artist.images[0].url : "/assets/" + ALBUM_PLACEHOLDER);
+    const imageUrl = (artist.images && artist.images.length ? artist.images[0].url : ALBUM_PLACEHOLDER);
 
     return (
       <div className="col-md-2">
         <div className="thumbnail">
           <img src={imageUrl} className="img-responsive center-block fxd-img"/>
-          <div className="caption">
-            <h4 className="">{artist.name}</h4>
-            <ul>
-              <li><span>Popularity: {artist.popularity}</span></li>
-              <li><span>Genre: {artist.genres.length ? artist.genres[0] : 'unknown'}</span></li>
-            </ul>
-            <Link to={`/artist/${artist.id}`} className="btn btn-default btn-xs" role="button">More Info</Link>
-          </div>
+          {this.renderArtistData(artist)}
         </div>
       </div>
     );
--- test/components/ArtistCard.spec.js
+++ test/components/ArtistCard.spec.js
@@ -3,14 +3,15 @@ import {expect} from 'chai';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-addons-test-utils';
 import {Link} from 'react-router';
+import {ALBUM_PLACEHOLDER} from '../../src/constants/Config';
 
 const ARTIST_FIXTURE = require('../fixtures/artists').artists.items[0];
-
+ARTIST_FIXTURE.genres = ['electric-rock'];
 import ArtistCard from '../../src/components/ArtistCard';
 
-function setup() {
+function setup(data) {
   let props = {
-    artist: ARTIST_FIXTURE
+    artist: data.artist || {}
   };
 
   let renderer = ReactTestUtils.createRenderer();
@@ -23,11 +24,11 @@ function setup() {
   };
 }
 
-describe('ArtistCard component', () => {
+describe('ArtistCard fully populated component', () => {
 
   it('renders the component', () => {
 
-    const {output} = setup();
+    const {output} = setup({artist: ARTIST_FIXTURE});
     expect(output.type).to.equal('div');
     expect(output.props.className).to.equal('col-md-2');
     expect(output.props.children).to.be.an('object');
@@ -67,10 +68,56 @@ describe('ArtistCard component', () => {
 
   it('has an artist prop props', () => {
 
-    const {props} = setup();
+    const {props} = setup({artist: ARTIST_FIXTURE});
 
     expect(props).to.not.be.empty;
     expect(props.artist).to.equal(ARTIST_FIXTURE);
   });
+});
 
+describe('Artist Card minimal-data component', () => {
+  it('renders the component', () => {
+    const {output} = setup({artist: {url: ''}});
+
+    expect(output.type).to.equal('div');
+    expect(output.props.className).to.equal('col-md-2');
+    expect(output.props.children).to.be.an('object');
+
+    const thumbnail = output.props.children;
+    expect(thumbnail.type).to.equal('div');
+    expect(thumbnail.props.className).to.equal('thumbnail');
+    expect(thumbnail.props.children).to.be.an('array');
+
+    const [image, caption] = thumbnail.props.children;
+
+    expect(image.type).to.equal('img');
+    expect(image.props.src).to.equal(ALBUM_PLACEHOLDER);
+    expect(image.props.className).to.equal('img-responsive center-block fxd-img');
+
+    expect(caption.type).to.equal('div');
+    expect(caption.props.className).to.equal('caption');
+    expect(caption.props.children).to.be.an('array');
+    expect(caption.props.children).to.have.length(3);
+
+    const [header, list, link] = caption.props.children;
+
+    expect(header).to.equal(null);
+
+    expect(list.type).to.equal('ul');
+    expect(list.props.children).to.be.an('array');
+    expect(list.props.children).to.have.length(2);
+
+      const [popularityItem, genreItem] = list.props.children;
+      expect(popularityItem).to.equal(null);
+      expect(genreItem).to.equal(null);
+
+    expect(link).to.equal(null);
+  });
+
+  it('has an artist property in props', () => {
+    const {props} = setup({artist: {url: ''}});
+
+    expect(props).to.have.property('artist');
+    expect(props.artist).to.be.defined;
+  });
 });
